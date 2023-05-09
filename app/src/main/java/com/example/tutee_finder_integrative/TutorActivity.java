2d4b1b787f1dfa8ad05399406abb80cbd8161e0d
ormsng <ormessing96@gmail.com>
2023-05-09 22:04:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tutee_finder_integrative/TutorActivity.java
@@ -0,0 +1,13 @@
+package com.example.tutee_finder_integrative;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TutorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor);
+    }
+}
